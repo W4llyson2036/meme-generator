17af9d522ac0bc42f5848ac95af4ef4a0e6094cb
W4llyson2036 <wallysonivo0111@gmail.com>
2024-01-17 18:45:20 -0300

    Added button to remove favorite memes

--- src/pages/FavoriteMemeImage.jsx
+++ src/pages/FavoriteMemeImage.jsx
@@ -2,45 +2,34 @@ import React, { useState, useEffect } from "react";
 import "../style/FavoriteMemeImage.css";
 
 export default function FavoriteMemeImage() {
-  const [renderComponent, setRenderComponent] = useState(false);
   const [favoriteMeme, setFavoriteMeme] = useState([]);
 
   useEffect(() => {
     const getMemeFromLocalStorage = JSON.parse(localStorage.getItem("memeList")) || [];
     setFavoriteMeme(getMemeFromLocalStorage);
-
-    const timeoutId = setTimeout(() => {
-      setRenderComponent(true);
-    }, 320);
-
-    // Limpa o temporizador no momento em que o componente é desmontado
-    return () => clearTimeout(timeoutId);
   }, []);
 
-  let memeElements = null;
-
   function removeMeme(indexToRemoveMeme) {
-    let newArray = favoriteMeme.filter(item => item.id !== indexToRemoveMeme)
-   
-    setFavoriteMeme(newArray)
-    removeFromLocalStorage(newArray)
+    const newArray = favoriteMeme.filter(item => item.id !== indexToRemoveMeme);
+    setFavoriteMeme(newArray);
+    localStorage.setItem('memeList', JSON.stringify(newArray));
   }
 
-  function removeFromLocalStorage(newArray) {
-    localStorage.setItem('memeList', JSON.stringify(newArray))
-  }  
-
-  if (favoriteMeme.length > 0 && renderComponent) {
+  let memeElements = null;
+  
+  if (favoriteMeme.length > 0) {
     memeElements = favoriteMeme.map((meme) => (
       <div className="container--favorite-meme" key={meme.id}>
         <p className="text-top">{meme.textTop}</p>
         <img className="meme-image-favorite" src={meme.url} alt={meme.id} />
         <p className="text-bottom">{meme.textBottom}</p>
-
         <div className="buttons">
-          {/* <button type="button" className="button-favorite">Edit</button>
-          <button className="button-favorite">Download</button> */}
-          <button className="button-favorite remove" onClick={() => removeMeme(meme.id)}>Remove</button>
+          <button 
+            className="button-favorite remove"
+            onClick={() => removeMeme(meme.id)}
+          >
+            Remove
+          </button>
         </div>
       </div>
     ));
@@ -49,14 +38,17 @@ export default function FavoriteMemeImage() {
   return (
     <div>
       {favoriteMeme.length > 0 ? (
-        <div className="grid-favorite-meme">{memeElements}</div>
+        <div className="grid-favorite-meme">
+          {memeElements}
+        </div>
       ) : (
-        renderComponent && (
-          <div className="container--without-favorite-meme">
-            <h1 className="without-favorite-meme">You don't have favorite meme</h1>
-            <img src="/emoticonSadIcon.svg" alt="emoticon_sad_icon.svg" />
-          </div>
-        )
+        <div className="container--without-favorite-meme">
+          <h1 className="without-favorite-meme">You don't have favorite meme</h1>
+          <img
+            src="/emoticonSadIcon.svg"
+            alt="emoticon_sad_icon.svg"
+          />
+        </div>
       )}
     </div>
   );
